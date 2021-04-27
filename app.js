048affe28100c3f06d458aa291df85c00412809a
Kamali Means <darenkmeans@gmail.com>
2021-04-27 15:50:36 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/* constants */
+/* app's state (variables) */
+// the numbers object can hold the state values of the current, previous, and sum Numbers
+let numbers = {
+  
+}
+/* cached element references */
+/* event listeners */
+/* functions */
